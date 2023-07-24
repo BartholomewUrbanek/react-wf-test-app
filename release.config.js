9771c2640aa9326f3d473220a254fac8ac54d3a2
Bartlomiej Urbanek <Bartlomiej.Urbanek@hyland.com>
2023-07-24 11:34:18 +0200

    fix: release assets

--- release.config.js
+++ release.config.js
@@ -1,5 +1,11 @@
 module.exports = {
     branches: ['main'],
     repositoryUrl: "https://github.com/BartholomewUrbanek/react-wf-test-app.git",
-    plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/npm', '@semantic-release/github']
+    plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/npm', 
+      ['@semantic-release/github', {
+        assets: [
+              {path: "build.zip", label: "Build" }
+        ]
+      }]
+    ]
 };
