07aec1101838fda1d5bf0f721442666a38259d01
Bartlomiej Urbanek <Bartlomiej.Urbanek@hyland.com>
2023-07-24 10:49:24 +0200

    fix: fix typo

--- release.config.js
+++ release.config.js
@@ -1,4 +1,4 @@
-module.ex = {
+module.exports = {
     branches: ['main'],
     repositoryUrl: "https://github.com/BartholomewUrbanek/react-wf-test-app",
     plugins: [['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/npm', '@semantic-release/github']]
